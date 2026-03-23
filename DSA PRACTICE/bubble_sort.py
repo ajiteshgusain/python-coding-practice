52bac04de0f6cf640d6960aa84801864125ccec0
Ajit <ajiteshgusain@gmail.com>
2026-03-23 19:41:21 +0530

    commit

--- DSA PRACTICE/bubble_sort.py
+++ DSA PRACTICE/bubble_sort.py
@@ -3,11 +3,19 @@
 # they  are in not proper order.
 list=[15,6,13,22,3,52,2]
 
+
+
+#bubble sort 
+#the basic idea of the bubble sort is to compare two adjoining elements and exchange them if
+# they  are in not proper order.
+list=[15,6,13,22,3,52,2]
+
 def bubble_sort(list):
     n=len(list)
     count=0
     #traverse through all list elements
     for i in range(0,n):
+        
         #last i elements  are already in place
         for j in range(0,n-i-1):
 
@@ -16,9 +24,61 @@ def bubble_sort(list):
             if list[j]>list[j+1]:
                 list[j],list[j+1]=list[j+1],list[j]
 
-            count+=1
+         
 
-            print("sorted list using bubble sort:",list)
+    
+
+    return list
+
+
+        #print("sorted list using bubble sort:",list)
+    #print("no of iterations:",count)
+
+
+
+
+
+print(bubble_sort(list))
+
+
+
+
+
+#optimized  bubble sort
+
+
+
+
+
+
+
+
+
+
+def bubble_sort(list):
+    n=len(list)
+    count=0
+    #traverse through all list elements
+    for i in range(0,n):
+        swapped=False
+        #last i elements  are already in place
+        for j in range(0,n-i-1):
+
+        #this expression  will ensure that we  do not compare 
+        # the heavier elements that have already settled at correct place.
+            if list[j]>list[j+1]:
+                list[j],list[j+1]=list[j+1],list[j]
+
+                swapped=True
+
+        if not swapped:
+            break
+    
+
+    return list
+
+
+        #print("sorted list using bubble sort:",list)
     #print("no of iterations:",count)
 
 
