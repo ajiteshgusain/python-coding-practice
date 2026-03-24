ba1dde50ff28674be5cbc713039b770b0d96da48
Ajit <ajiteshgusain@gmail.com>
2026-03-24 19:06:35 +0530

    commit

--- DSA PRACTICE/sorting techinques/selection_sort.py
+++ DSA PRACTICE/sorting techinques/selection_sort.py
@@ -1,5 +1,21 @@
 #selection sort: it works like this as it check one element at a time from the unsorted list
 # by inserting th element at its correct position in sorted list.
 
+def selection_sort(list):
+    for i in range(1,len(list)):
+        key=list[i]
+        j=i-1
+        while j>=0 and key<list[j]:
+            list[j+1]=list[j]
+            j=j-1
+        else:
+            list[j+1]=key
 
 
+
+
+
+
+
+
+list=[15,6,13,22,3,52,2]
