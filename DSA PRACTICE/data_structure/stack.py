28a7fb67e03c148df568dea7c912a879f4ad9a6a
Ajit <ajiteshgusain@gmail.com>
2026-03-28 10:23:33 +0530

    commit

---
+++ DSA PRACTICE/data_structure/stack.py
@@ -0,0 +1,24 @@
+#stack without using oop
+list1=[12]
+
+def list_pop(list1):
+    if   is_empty():
+        return f"list is empty underflow"
+    
+       
+
+    popped_element=list1.pop()
+    
+    return f"popped element :{popped_element}"
+
+
+def is_empty():
+    if len(list1)==0:
+        return True
+    
+
+print(list_pop(list1))
+print(list_pop(list1))
+
+
+
