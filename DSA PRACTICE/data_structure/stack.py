a53db4492bc8313af118afb054cdcb03683d2307
Ajit <ajiteshgusain@gmail.com>
2026-03-28 10:45:10 +0530

    learning stack

--- DSA PRACTICE/data_structure/stack.py
+++ DSA PRACTICE/data_structure/stack.py
@@ -2,7 +2,7 @@
 list1=[12]
 
 def list_pop(list1):
-    if   is_empty():
+    if   is_empty(list1):
         return f"list is empty underflow"
     
        
@@ -12,9 +12,21 @@ def list_pop(list1):
     return f"popped element :{popped_element}"
 
 
-def is_empty():
-    if len(list1)==0:
-        return True
+def is_empty(list1):
+    
+    #normal 
+    # #=========================
+    # this python statement works like this agar list ki len =0 hai means true
+    # if len(list1)==0:
+    #     return True
+    
+    # False
+    ##========================================================
+    # a more simplified version 
+    return len(list1)==0 #it will return true if list empty and false if not
+
+    # a more professional version
+    #will be as we know a empty list is considered as  false then we use
     
 
 print(list_pop(list1))
