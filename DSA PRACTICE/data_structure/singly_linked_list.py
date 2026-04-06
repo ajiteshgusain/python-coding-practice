edbc46ed2eb2e509a36c30f3d8a089135332f7e9
Ajit <ajiteshgusain@gmail.com>
2026-04-06 12:49:00 +0530

    working on linked list

---
+++ DSA PRACTICE/data_structure/singly_linked_list.py
@@ -0,0 +1,6 @@
+# The Anatomy of a Node
+# Before we build the list, we need to build the Node. Think of a Node as a person in a treasure hunt holding a piece of paper:
+
+# Data: The actual treasure (the value).
+
+# Next: The map telling them who the next person in the hunt is.
