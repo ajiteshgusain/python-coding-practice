0d4141ba87eba0d9db6bdf3a1f674f00f53eec2e
Ajit <ajiteshgusain@gmail.com>
2026-06-05 20:43:25 +0530

    Refactor turtle graphics code: update background color to black and adjust hue initialization for vibrant pattern generation

--- DSA PRACTICE/Fascinating python code/colourful pattern/fascinating_code_2.py
+++ DSA PRACTICE/Fascinating python code/colourful pattern/fascinating_code_2.py
@@ -1,5 +1,24 @@
-from turtle import colorsys
+from turtle import *
+import colorsys 
 
 speed(0)
-hue=0.5
-bgcolor('red')
+bgcolor('black')
+hue=0.0
+for i in range(130):
+    color_rgb=colorsys.hsv_to_rgb(hue, 1, 1)
+    color(color_rgb)
+
+    hue +=0.005
+
+    begin_fill()
+
+    circle(149,90)
+
+    lt(100)
+
+
+    circle(149 , 90)
+    
+    end_fill()
+
+done()
